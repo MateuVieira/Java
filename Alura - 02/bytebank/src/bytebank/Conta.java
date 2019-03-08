09694a7dcfb561f92b707de1af97fa3d8b101bf9
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-03-08 18:25:35 -0300

    Bytebank - aula 2

---
+++ Alura - 02/bytebank/src/bytebank/Conta.java
@@ -0,0 +1,9 @@
+package bytebank;
+
+public class Conta {
+
+	double saldo = 0;
+	int agencia;
+	int numero;
+	String titular;
+}
---
+++ Alura - 02/bytebank/src/bytebank/CriaConta.java
@@ -0,0 +1,18 @@
+package bytebank;
+
+public class CriaConta {
+	
+	public static void main(String[] args) {
+		
+		Conta primeriraConta = new Conta();
+		primeriraConta.saldo = 200;
+		
+		Conta segundaConta = new Conta();
+		segundaConta.saldo = 50;
+		
+		System.out.println("primeira conta tem " + primeriraConta.saldo);
+		System.out.println("segunda conta tem " + segundaConta.saldo);
+		
+	}
+
+}
