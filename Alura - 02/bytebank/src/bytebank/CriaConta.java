f4450dfd677fce102c8fc3abc9c2fd69f6aabedf
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-03-08 23:44:00 -0300

    Update CriaConta.java

--- Alura - 02/bytebank/src/bytebank/CriaConta.java
+++ Alura - 02/bytebank/src/bytebank/CriaConta.java
@@ -5,13 +5,13 @@ public class CriaConta {
 	public static void main(String[] args) {
 		
 		Conta primeriraConta = new Conta();
-		primeriraConta.saldo = 200;
+		//primeriraConta.saldo = 200;
 		
 		Conta segundaConta = new Conta();
-		segundaConta.saldo = 50;
+		//segundaConta.saldo = 50;
 		
-		System.out.println("primeira conta tem " + primeriraConta.saldo);
-		System.out.println("segunda conta tem " + segundaConta.saldo);
+		//System.out.println("primeira conta tem " + primeriraConta.saldo);
+		//System.out.println("segunda conta tem " + segundaConta.saldo);
 		
 	}
 
