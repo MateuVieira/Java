15709ec2e7086344c2dff1363cd3f07c370c42ed
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-03-09 14:41:05 -0300

    Create SistemaInterno.java

---
+++ Alura - 03/bytebank-heranca/src/SistemaInterno.java
@@ -0,0 +1,14 @@
+
+public class SistemaInterno {
+
+	private int senha = 2222;
+	
+	public void autentica(Autenticavel fa) {
+		
+		boolean autentica = fa.autentica(this.senha);
+		if (autentica) {
+			System.out.println("Pode entrar no Sistema");
+		}
+		System.out.println("Não pode entrar no Sistema");
+	}
+}
