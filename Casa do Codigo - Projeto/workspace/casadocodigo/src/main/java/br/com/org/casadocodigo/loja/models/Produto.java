4d8d361984893b7fe60e1ad72afbedf0c455975c
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-03-21 16:38:34 -0300

    Create Produto.java

---
+++ Casa do Codigo - Projeto/workspace/casadocodigo/src/main/java/br/com/org/casadocodigo/loja/models/Produto.java
@@ -0,0 +1,34 @@
+package br.com.org.casadocodigo.loja.models;
+
+public class Produto {
+
+	private String titulo;
+	private String descricao;
+	private String paginas;
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getPaginas() {
+		return paginas;
+	}
+	public void setPaginas(String paginas) {
+		this.paginas = paginas;
+	}
+	
+	@Override
+	public String toString() {
+		return "Produto [titulo=" + titulo + ", descricao=" + descricao + ", paginas=" + paginas + "]";
+	}
+	
+	
+}
