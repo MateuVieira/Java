938ae3d98bcccbd94cdbc428fe6cd5133f2dfaa7
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-03-21 17:19:10 -0300

    Create JPAConfiguration.java

---
+++ Casa do Codigo - Projeto/workspace/casadocodigo/src/main/java/br/com/org/casadocodigo/loja/conf/JPAConfiguration.java
@@ -0,0 +1,44 @@
+package br.com.org.casadocodigo.loja.conf;
+
+import java.util.Map;
+import java.util.Properties;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.spi.PersistenceProvider;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaDialect;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+public class JPAConfiguration {
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+		
+		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		factoryBean.setJpaVendorAdapter(vendorAdapter);
+		
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setUsername("root");
+		dataSource.setPassword("root");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/casadocodigo");
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		factoryBean.setDataSource(dataSource);
+		
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+		properties.setProperty("hibernate.show_sql", "true");
+		properties.setProperty("hibernate.hbm2ddl.auto", "upadate");
+		factoryBean.setJpaProperties(properties);
+		
+		factoryBean.setPackagesToScan("br.com.org.casadocodigo.loja.models");
+		
+		return factoryBean;
+	}
+	
+}
