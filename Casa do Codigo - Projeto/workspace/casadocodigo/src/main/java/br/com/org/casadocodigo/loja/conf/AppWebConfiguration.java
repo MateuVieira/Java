2da981227f292c2e062df2e2618e687b8467789b
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-03-21 15:05:03 -0300

    Update AppWebConfiguration.java

--- Casa do Codigo - Projeto/workspace/casadocodigo/src/main/java/br/com/org/casadocodigo/loja/conf/AppWebConfiguration.java
+++ Casa do Codigo - Projeto/workspace/casadocodigo/src/main/java/br/com/org/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -1,5 +1,13 @@
 package br.com.org.casadocodigo.loja.conf;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import br.com.org.casadocodigo.loja.controllers.HomeController;
+
+@EnableWebMvc
+@ComponentScan(basePackageClasses = {HomeController.class})
 public class AppWebConfiguration {
 
+	
 }
