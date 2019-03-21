b7fa9ee237df99af514107c24195fc56f2fd9d94
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-03-21 16:49:42 -0300

    Update ProdutosController.java

--- Casa do Codigo - Projeto/workspace/casadocodigo/src/main/java/br/com/org/casadocodigo/loja/controllers/ProdutosController.java
+++ Casa do Codigo - Projeto/workspace/casadocodigo/src/main/java/br/com/org/casadocodigo/loja/controllers/ProdutosController.java
@@ -3,11 +3,14 @@ package br.com.org.casadocodigo.loja.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.org.casadocodigo.loja.daos.ProdutoDAO;
 import br.com.org.casadocodigo.loja.models.Produto;
 
 @Controller
 public class ProdutosController {
-
+	
+	private ProdutoDAO produtoDao;
+	
 	@RequestMapping("/produtos/form")
 	public String form() {
 		return "produtos/form";
