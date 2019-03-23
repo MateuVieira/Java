7946824254d2f23c6adde615fb76de1adb62d3ca
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-03-22 23:39:01 -0300

    Update CarrinhoCompras.java

--- Casa do Codigo - Projeto/workspace/casadocodigo/src/main/java/br/com/casadocodigo/loja/models/CarrinhoCompras.java
+++ Casa do Codigo - Projeto/workspace/casadocodigo/src/main/java/br/com/casadocodigo/loja/models/CarrinhoCompras.java
@@ -23,14 +23,14 @@ public class CarrinhoCompras {
 		itens.put(item, getQuantidade(item) + 1);
 	}
 
-	private int getQuantidade(CarrinhoItem item) {
+	public int getQuantidade(CarrinhoItem item) {
 		if(!itens.containsKey(item)) {
 			itens.put(item, 0);
 		}
 		return itens.get(item);
 	}
 	
-	private int getQuantidade() {
+	public int getQuantidade() {
 		return itens.values().stream().reduce(0, (proximo, acumulador) -> proximo + acumulador);
 	}
 	
