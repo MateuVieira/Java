5e82cf17fa72fc31ae2c4a420b4a0b1b0b0a42ba
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-03-22 23:35:08 -0300

    Update CarrinhoComprasControllers.java

--- Casa do Codigo - Projeto/workspace/casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/CarrinhoComprasControllers.java
+++ Casa do Codigo - Projeto/workspace/casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/CarrinhoComprasControllers.java
@@ -1,9 +1,12 @@
 package br.com.casadocodigo.loja.controllers;
 
+import java.io.Serializable;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -16,7 +19,12 @@ import br.com.casadocodigo.loja.models.TipoPreco;
 @Controller
 @RequestMapping("/carrinho")
 @Scope(value=WebApplicationContext.SCOPE_REQUEST)
-public class CarrinhoComprasControllers {
+public class CarrinhoComprasControllers implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	@Autowired
 	private ProdutoDAO produtoDAO;
@@ -26,7 +34,7 @@ public class CarrinhoComprasControllers {
 
 	@RequestMapping("/add")
 	public ModelAndView add(Integer produtoId, TipoPreco tipoPreco) {
-		ModelAndView modelAndView = new ModelAndView("redirect:/produtos");
+		ModelAndView modelAndView = new ModelAndView("redirect:/carrinho");
 		CarrinhoItem carrinhoItem = criaItem(produtoId, tipoPreco);
 		carrinho.add(carrinhoItem);
 		return modelAndView;
@@ -36,5 +44,10 @@ public class CarrinhoComprasControllers {
 		Produto produto = produtoDAO.find(produtoId);
 		CarrinhoItem carrinhoItem = new CarrinhoItem(produto, tipoPreco);
 		return carrinhoItem;
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ModelAndView itens() {
+		return new ModelAndView("/carrinho/itens");
 	}
 }
