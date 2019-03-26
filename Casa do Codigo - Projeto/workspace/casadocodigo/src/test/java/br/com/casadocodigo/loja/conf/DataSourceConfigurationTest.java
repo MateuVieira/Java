ea735cef44450e4ad17e4c774fa5341045f37a05
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-03-26 01:23:34 -0300

    Update DataSourceConfigurationTest.java

--- Casa do Codigo - Projeto/workspace/casadocodigo/src/test/java/br/com/casadocodigo/loja/conf/DataSourceConfigurationTest.java
+++ Casa do Codigo - Projeto/workspace/casadocodigo/src/test/java/br/com/casadocodigo/loja/conf/DataSourceConfigurationTest.java
@@ -2,10 +2,10 @@ package br.com.casadocodigo.loja.conf;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
 
 public class DataSourceConfigurationTest {
 
