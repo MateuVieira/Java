4549fcc189d5a0229502115f96c8fac4aa510f85
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-03-26 13:50:21 -0300

    Create HomeControllerTest.java

---
+++ Casa do Codigo - Projeto/workspace/casadocodigo/src/test/java/br/com/casadocodigo/loja/Controllers/HomeControllerTest.java
@@ -0,0 +1,51 @@
+package br.com.casadocodigo.loja.Controllers;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import br.com.casadocodigo.loja.conf.AppWebConfiguration;
+import br.com.casadocodigo.loja.conf.DataSourceConfigurationTest;
+import br.com.casadocodigo.loja.conf.JPAConfiguration;
+import br.com.casadocodigo.loja.dao.ProdutoDAO;
+import br.com.casadocodigo.loja.dao.UsuarioDAO;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes= {
+		AppWebConfiguration.class,
+		JPAConfiguration.class,
+		ProdutoDAO.class,
+		DataSourceConfigurationTest.class,
+		UsuarioDAO.class,
+		})
+@ActiveProfiles("test")
+public class HomeControllerTest {
+
+	private MockMvc mockMvc;
+	@Autowired
+	private WebApplicationContext wac;
+	
+	@Before
+	public void setup() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+	}
+	
+	@Test
+	public void deveRetornarParaHomeComOsLivros() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/"))
+		.andExpect(MockMvcResultMatchers.model().attributeExists("produtos"))
+		.andExpect(MockMvcResultMatchers.forwardedUrl("/WEB-INF/views/Home.jsp"));
+	}
+}
