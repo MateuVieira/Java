e893b85a13278889126e8b82e3e7bdfa7a0309ae
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-03-26 17:39:08 -0300

    Update ProdutoDAO.java

--- Casa do Codigo - Projeto/workspace/casadocodigo/src/main/java/br/com/casadocodigo/loja/dao/ProdutoDAO.java
+++ Casa do Codigo - Projeto/workspace/casadocodigo/src/main/java/br/com/casadocodigo/loja/dao/ProdutoDAO.java
@@ -27,7 +27,7 @@ public class ProdutoDAO {
 
 	public List<Produto> listar() {
 		
-		return manager.createQuery("select  distinct(p) from Produto p join fetch p.precos", Produto.class).getResultList();
+		return manager.createQuery("select  distinct(p) from Produto p", Produto.class).getResultList();
 	}
 
 	public Produto find(Integer id) {
