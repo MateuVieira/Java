4a99f763b5f3cb9961e1628b1cfae0a7f549242d
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-03-26 10:31:28 -0300

    Update ProdutoDAOTest.java

--- Casa do Codigo - Projeto/workspace/casadocodigo/src/test/java/br/com/casadocodigo/loja/dao/ProdutoDAOTest.java
+++ Casa do Codigo - Projeto/workspace/casadocodigo/src/test/java/br/com/casadocodigo/loja/dao/ProdutoDAOTest.java
@@ -18,30 +18,27 @@ import br.com.casadocodigo.loja.models.Produto;
 import br.com.casadocodigo.loja.models.TipoPreco;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes= {JPAConfiguration.class, ProdutoDAO.class})
+@ContextConfiguration(classes = { JPAConfiguration.class, UsuarioDAO.class, })
 @ActiveProfiles("test")
 public class ProdutoDAOTest {
 
 	@Autowired
 	private ProdutoDAO produtoDao;
-	
+
 	@Test
 	@Transactional
 	public void deveSomarTodosPrecosPorTipoLivro() {
-		
-		List<Produto> livrosImpressos = ProdutoBuilder
-										.newProduto(TipoPreco.IMPRESSO, BigDecimal.TEN)
-										.more(3).buildAll();
-		
-		List<Produto> livrosEbook = ProdutoBuilder
-				.newProduto(TipoPreco.EBOOK, BigDecimal.TEN)
-				.more(3).buildAll();
-		
+
+		List<Produto> livrosImpressos = ProdutoBuilder.newProduto(TipoPreco.IMPRESSO, BigDecimal.TEN).more(3)
+				.buildAll();
+
+		List<Produto> livrosEbook = ProdutoBuilder.newProduto(TipoPreco.EBOOK, BigDecimal.TEN).more(3).buildAll();
+
 		livrosImpressos.stream().forEach(produtoDao::gravar);
 		livrosEbook.stream().forEach(produtoDao::gravar);
-		
+
 		BigDecimal valor = produtoDao.somaPrecosPorTipo(TipoPreco.EBOOK);
 		Assert.assertEquals(new BigDecimal(40).setScale(2), valor);
-		
+
 	}
 }
