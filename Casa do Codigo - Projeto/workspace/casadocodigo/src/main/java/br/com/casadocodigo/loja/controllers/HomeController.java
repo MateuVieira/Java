706cf34e4f45ee0f4bb92b04ee97f15884831700
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-03-26 23:28:40 -0300

    Update HomeController.java

--- Casa do Codigo - Projeto/workspace/casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/HomeController.java
+++ Casa do Codigo - Projeto/workspace/casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/HomeController.java
@@ -1,21 +1,29 @@
 package br.com.casadocodigo.loja.controllers;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.casadocodigo.loja.dao.ProdutoDAO;
+import br.com.casadocodigo.loja.dao.UsuarioDAO;
 import br.com.casadocodigo.loja.models.Produto;
+import br.com.casadocodigo.loja.models.Role;
+import br.com.casadocodigo.loja.models.Usuario;
 
 @Controller
 public class HomeController {
 
 	@Autowired
 	private ProdutoDAO produtoDao;
+	@Autowired
+	private UsuarioDAO usuarioDao;
 	
 	@RequestMapping("/")
 	@Cacheable(value="produtoHome")
@@ -25,5 +33,18 @@ public class HomeController {
 		modelAndView.addObject("produtos", produtos);
 		
 		return modelAndView;
+	}
+	
+	@Transactional
+	@ResponseBody
+	@RequestMapping("/url-magica-maluca-fhdoaiudshgpiuhgudHPUhgpidUHGPUDOIDUIOapth")
+	public String urlMagicaMaluca( ) {
+		Usuario usuario = new Usuario();
+		usuario.setNome("Admin");
+	    usuario.setEmail("admin@casadocodigo.com.br");
+	    usuario.setSenha("$2a$10$lt7pS7Kxxe5JfP.vjLNSyOXP11eHgh7RoPxo5fvvbMCZkCUss2DGu");
+	    usuario.setRoles(Arrays.asList(new Role("ADMIN")));
+		usuarioDao.gravar(usuario);
+		return "Url Mágica excutada";
 	}
 }
