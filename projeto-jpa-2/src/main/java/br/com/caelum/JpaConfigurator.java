a9e9ccb7acff7a3d2f65171315f564d23116730f
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-03-27 01:16:26 -0300

    Update JpaConfigurator.java

--- projeto-jpa-2/src/main/java/br/com/caelum/JpaConfigurator.java
+++ projeto-jpa-2/src/main/java/br/com/caelum/JpaConfigurator.java
@@ -8,7 +8,6 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
