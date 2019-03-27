d47ad551446c07c626d876338730c69c6f2f70ee
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-03-27 01:15:59 -0300

    Update JPAProductionConfiguration.java

--- Casa do Codigo - Projeto/workspace/casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/JPAProductionConfiguration.java
+++ Casa do Codigo - Projeto/workspace/casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/JPAProductionConfiguration.java
@@ -1,15 +1,21 @@
 package br.com.casadocodigo.loja.conf;
 
+import java.beans.PropertyVetoException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Properties;
 
+import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+//import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 
 @Profile("prod")
 public class JPAProductionConfiguration {
@@ -22,21 +28,25 @@ public class JPAProductionConfiguration {
 		Properties properties = new Properties();
 		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
 		properties.setProperty("hibernate.show_sql", "true");
-		properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+		properties.setProperty("hibernate.hbm2ddl.auto", "update");
 		return properties;
 	}
 
 	@Bean
-	public DriverManagerDataSource dataSource() throws URISyntaxException {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("org.postgresql.Driver");
+	public DataSource getDataSource() throws PropertyVetoException, URISyntaxException {
+		ComboPooledDataSource dataSource = new ComboPooledDataSource();
+		dataSource.setDriverClass("org.postgresql.Driver");
 
 		//usuario:senha@host:port/path
 		URI dbURL = new URI(environment.getProperty("DATABASE_URL"));
-		dataSource.setUsername(dbURL.getUserInfo().split(":")[0]);
+		dataSource.setUser(dbURL.getUserInfo().split(":")[0]);
 		dataSource.setPassword(dbURL.getUserInfo().split(":")[1]);
-		dataSource.setUrl("jdbc:postgresql://" + dbURL.getHost()
+		dataSource.setJdbcUrl("jdbc:postgresql://" + dbURL.getHost()
 				+ ":" + dbURL.getPort() + dbURL.getPath());
+		dataSource.setMinPoolSize(1);
+		dataSource.setMaxPoolSize(2);
+		//dataSource.setNumHelperThreads(10);
+		
 		return dataSource;
 	}
 }
